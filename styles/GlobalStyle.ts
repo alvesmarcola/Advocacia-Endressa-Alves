8755cc63fef13e09319dfc750cc01bc1857055d1
alvesmarcola <alvesbrunom8@gmail.com>
2026-09-23 11:48:05 -0300

    New changes

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,58 +1,68 @@
 import { createGlobalStyle } from 'styled-components'
+import { defaultTheme } from './themes/defaultTheme'
 
 export const GlobalStyle = createGlobalStyle`
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: ${({ theme }) => defaultTheme.offWhite};
+    color: ${({ theme }) => defaultTheme.coffee};
+
+    overflow-x: hidden;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html {
+    scroll-behavior: smooth;
+
+    @media (max-width: 1088px) {
+      font-size: 93.75%;
     }
 
-    body {
-        background: #FFFFFF;
-        color: #fff;
-
-        overflow-x: hidden;
-        -webkit-font-smoothing: antialiased;
+    @media (max-width: 720px) {
+      font-size: 87.5%;
     }
 
-    html {
-        @media (max-width: 1088px) {
-            font-size: 93.75%;
-        }
-        @media (max-width: 720px) {
-            font-size: 87.5%;
-        }
-        scroll-behavior: smooth;
-
-        &::-webkit-scrollbar {
-        border-radius: 8px;
-        width: 0.5rem;
-        background: #fff ;
+    &::-webkit-scrollbar {
+      border-radius: 8px;
+      width: 0.5rem;
+      background: ${({ theme }) => defaultTheme.beige};
     }
+
     &::-webkit-scrollbar-thumb {
-        border-radius: 8px;
-        background: #0A3D62 ;
+      border-radius: 8px;
+      background: ${({ theme }) => defaultTheme.plum};
     }
+  }
 
-    body, input, textarea, button {
-        font-family: 'roboto', sans-serif;
-        font-weight: 400;
-        font-size: 1rem;
-    }
+  body,
+  input,
+  textarea,
+  button {
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
 
-    h1 {
-        font-family: 'Lato';
-        font-weight: 400;
-        font-size: 3.5rem;
-        line-height: 125%
-    }
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: 'Playfair Display', serif;
+    font-weight: 400;
+  }
 
-    li {
-            list-style-type: none;
-    }
+  li {
+    list-style-type: none;
+  }
 
-    a {
-        text-decoration: none;
-
-    }
+  a {
+    text-decoration: none;
+  }
 `
--- styles/themes/defaultTheme.ts
+++ styles/themes/defaultTheme.ts
@@ -1,28 +1,37 @@
 export const defaultTheme = {
-  gray900: '#2B211C',
-  gray800: '#3D2A20',
-  gray700: '#5C3A28',
-  gray600: '#6E4A35',
-  gray500: '#8A6652',
-  gray400: '#A8836D',
-  gray300: '#C7AA98',
-  gray200: '#D8C5B7',
-  gray100: '#FFFDFC',
-  gray050: '#F5EEE8',
+  // Cores principais
+  offWhite: "#F1E5E8",
+  beige: "#E4D6D9",
+  mauve: "#CDBBC2",
+  rose: "#BB98A5",
+  plum: "#6E5064",
+  coffee: "#573C2C",
+
+  // Cores antigas
+  gray900: "#2B211C",
+  gray800: "#3D2A20",
+  gray700: "#5C3A28",
+  gray600: "#6E4A35",
+  gray500: "#8A6652",
+  gray400: "#A8836D",
+  gray300: "#C7AA98",
+  gray200: "#D8C5B7",
+  gray100: "#FFFDFC",
+  gray050: "#F5EEE8",
 
   gradientDark:
-    'linear-gradient(90deg, rgba(110, 47, 21, 0) 0%, rgba(110, 47, 21, 0.24) 100%)',
+    "linear-gradient(90deg, rgba(110, 47, 21, 0) 0%, rgba(110, 47, 21, 0.24) 100%)",
 
   gradientSection:
-    'linear-gradient(180deg, #9A451B 0%, rgba(154, 69, 27, 0) 100%)',
+    "linear-gradient(180deg, #9A451B 0%, rgba(154, 69, 27, 0) 100%)",
 
   gradientLight:
-    'linear-gradient(329.9deg, rgba(154, 69, 27, 0.08) 14.05%, rgba(154, 69, 27, 0.03) 87%)',
+    "linear-gradient(329.9deg, rgba(154, 69, 27, 0.08) 14.05%, rgba(154, 69, 27, 0.03) 87%)",
 
   gradientBorder:
-    'linear-gradient(137.62deg, rgba(154, 69, 27, 0.2) 1.79%, rgba(154, 69, 27, 0) 97.61%)',
+    "linear-gradient(137.62deg, rgba(154, 69, 27, 0.2) 1.79%, rgba(154, 69, 27, 0) 97.61%)",
 
-  primaryDark: '#6E2F15',
-  primaryDefault: '#9A451B',
-  primaryLight: '#B96535',
-}
+  primaryDark: "#6E2F15",
+  primaryDefault: "#9A451B",
+  primaryLight: "#B96535",
+};
