4dd70346a35d8d479339f4a74f75999415dafaa4
alvesmarcola <alvesbrunom8@gmail.com>
2026-09-23 14:22:15 -0300

    Ok

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -4,10 +4,12 @@ import { Home } from '../Home'
 import { LeadCapture } from '../LeadCapture'
 import { Services } from '../Services'
 import { MainContainer } from './style'
+import { Sidebar } from '../Sidebar'
 
 export function Main() {
   return (
     <MainContainer>
+      <Sidebar />
       <Home />
       <Services />
       <CTA />
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,81 +1,14 @@
-import linkedinImg from '../../assets/in_white.svg'
-import faceImg from '../../assets/face-white.svg'
-import instaImg from '../../assets/insta-white.svg'
-import { Sidebar as SidebarContainer } from 'primereact/sidebar'
-import { SidebarButton, SidebarContent } from './style'
-import { useState } from 'react'
-import 'primereact/resources/themes/lara-light-indigo/theme.css' // theme
-import 'primereact/resources/primereact.min.css' // core css
-import 'primeicons/primeicons.css'
-import hambImg from '../../assets/hamb.svg'
+import { SidebarContainer } from './style'
 
 export function Sidebar() {
-  const [visibleLeft, setVisibleLeft] = useState(false)
-
   return (
-    <>
-      <SidebarButton
-        src={hambImg}
-        onClick={() => setVisibleLeft(true)}
-        alt="mostrar sidebar"
-      ></SidebarButton>
-      <SidebarContainer
-        style={{ background: '#0A3D62' }}
-        visible={visibleLeft}
-        fullScreen
-        onHide={() => setVisibleLeft(false)}
-      >
-        <SidebarContent>
-          <nav>
-            <a href="#contato" onClick={() => setVisibleLeft(false)}>
-              Contato
-            </a>
-            <a href="#services" onClick={() => setVisibleLeft(false)}>
-              Áreas de atuação
-            </a>
-            <a href="#home" onClick={() => setVisibleLeft(false)}>
-              Home
-            </a>
-            <a
-              href="https://wa.me/5499359083"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Entrar em contato
-            </a>
-
-            <nav className="socialNav">
-            <a
-            target="_blank"
-            href="https://www.linkedin.com/in/"
-            rel="noreferrer"
-          >
-            <img width={40} height={40} src={linkedinImg} alt="linkedin" />
-          </a>
-          <a
-            target="_blank"
-            href="https://www.facebook.com/endressa.alves"
-            rel="noreferrer"
-          >
-            <img width={40} height={40} src={faceImg} alt="facebook" />
-          </a>
-          <a
-            target="_blank"
-            href="https://www.instagram.com/endressalves?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="
-            rel="noreferrer"
-          >
-            <img width={40} height={40} src={instaImg} alt="instagram" />
-          </a>
-            </nav>
-          </nav>
-          <footer>
-            <img width={90}  alt="logomarca" />
-            <p>
-              Social media.
-            </p>
-          </footer>
-        </SidebarContent>
-      </SidebarContainer>
-    </>
+    <SidebarContainer
+      href="https://wa.me/5554999359083"
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label="Falar pelo WhatsApp"
+    >
+      <span>WhatsApp</span>
+    </SidebarContainer>
   )
 }
